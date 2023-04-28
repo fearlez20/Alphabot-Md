11411891fab3d5cceacc6b91c52c993bf655ced5
fearlez20 <126472329+fearlez20@users.noreply.github.com>
2023-04-28 23:43:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,24 +31,24 @@ global.language = english //change indonesia to english if you don't understand 
 
 // Other
 // Other
-global.botname = "Alphabot-Md" //namabot kalian
+global.botname = "UCHIHA-Md" //namabot kalian
 global.ownername = "FEARLEZ" //nama kalian
 global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
-global.myweb2 = "https://s.id/BOTGITHUB" //bebas
-global.email = "mdsandaru2@gmail.com" //bebas
-global.region = "Buddhist" //bebas
-global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['2349160422155'] //premium user
-global.owner = ["2349160422155", "2349160422155"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "2349160422155" // nomor wa kalian
-global.ownernomerr = "+2349160422155" //nmr wa kalian
+global.youtube = "https://youtube.com/@unknown" //bebas asal jan hapus
+global.myweb2 = "https://mandem" //bebas
+global.email = "ambrosekula25@gmail.com" //bebas
+global.region = "unknown" //bebas
+global.timezone = 'Africa/Colombo' //  timezone wib
+global.premium = ['2348131467919'] //premium user
+global.owner = ["2348131467919", "2349160422155"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "2348131467919" // nomor wa kalian
+global.ownernomerr = "+2348131467919" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Md' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© UCHIHA-Md' //sticker wm ubah
+global.author = 'xXxERENxXx' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
